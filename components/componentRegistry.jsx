753be125e376c4cccda7f0e324e0656a9be373fe
Blinkflo Builder <builder@blinkflo.dev>
2026-01-17 22:18:40 +0000

    auto generated

---
+++ components/componentRegistry.jsx
@@ -0,0 +1,42 @@
+
+import HeroSection from "./heroSection";
+import TargetAudienceSectionComp from "./targetAudienceSection";
+import GridVideo from "./GridVideo";
+import WhyChoose from "./whyChoose";
+import FeaturesSection from "./featuresSection";
+import TransformSection from "./TransformSection";
+import MetricsSection from "./key-metric";
+import IntegrationSection from "./integrationsSection";
+import PricingSection from "./PricingSection";
+import FAQSection from "./FaqSection";
+import TestimonialSection from "./testimonial";
+import AboutHeroSection from "./aboutHeroSection";
+import AboutMetric from "./aboutMetric";
+import AboutOurStory from "./aboutOurStory";
+import AboutOurTeam from "./aboutOurTeam";
+import AboutOurValues from "./aboutOurValues";
+import ContactSupportOptions from "./contactSupportOptions";
+import Header from "./header";
+import Footer from "./footer";
+
+export const componentRegistry = {
+  "hero-section": HeroSection,
+  "target-audience": TargetAudienceSectionComp,
+  "grid-video": GridVideo,
+  "why-choose": WhyChoose,
+  "feature-section": FeaturesSection,
+  "transform-section": TransformSection,
+  "metrics-section": MetricsSection,
+  "integration-section": IntegrationSection,
+  "pricing-section": PricingSection,
+  "faq-section": FAQSection,
+  "testimonials-section": TestimonialSection,
+  "about-hero": AboutHeroSection,
+  "about-metric": AboutMetric,
+  "about-our-story": AboutOurStory,
+  "about-our-team": AboutOurTeam,
+  "about-our-values": AboutOurValues,
+  "contact-support-options": ContactSupportOptions,
+  "header-sec": Header,
+  "footer-section": Footer
+};
